67e822f50358752d3927b64d3808e242c0f5dbe7
Taima Hamadneh <98472054+TaimaHamadneh@users.noreply.github.com>
2024-09-17 20:42:14 +0300

    feat(Update tests.py):  Create Automated Tests

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+# members/tests.py
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class MyViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello_view_without_name(self):
+        response = self.client.get(reverse('hello_view'))
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data['greeting'], "Hello, World")
+
+    def test_hello_view_with_name(self):
+        response = self.client.get(reverse('hello_view') + '?name=John')
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data['greeting'], "Hello, John")
+    
+    def test_info_view(self):
+        response = self.client.get(reverse('info_view'))
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+        self.assertIn('time', response_data)
+        self.assertIn('client_address', response_data)
+        self.assertIn('host_name', response_data)
+        self.assertIn('headers', response_data)
